c96a99d6cac23cd4690d66bf525c2d97ba71da1e
jayeshmax <38346906+jayeshmax@users.noreply.github.com>
2025-05-28 22:52:41 +0530

    gradle updated and now working

--- app/src/main/java/com/nexlatech/fagito/api/RetrofitHelper.kt
+++ app/src/main/java/com/nexlatech/fagito/api/RetrofitHelper.kt
@@ -4,7 +4,8 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitHelper {
-    private const val BASE_URL = "https://nexla-271516.uc.r.appspot.com/"
+//    private const val BASE_URL = "https://nexla-271516.uc.r.appspot.com/"
+    private const val BASE_URL = "http://192.168.56.1:80/"
 
     fun getInstance(): Retrofit{
         return Retrofit.Builder()
