c7f93ac9f38f28c16414e52e32e5464c016878d9
jayeshmax <38346906+jayeshmax@users.noreply.github.com>
2025-06-01 00:01:17 +0530

    working server request timeout increased by 60sec

--- app/src/main/java/com/nexlatech/fagito/api/RetrofitHelper.kt
+++ app/src/main/java/com/nexlatech/fagito/api/RetrofitHelper.kt
@@ -1,16 +1,26 @@
 package com.nexlatech.fagito.api
 
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
 
 object RetrofitHelper {
 //    private const val BASE_URL = "https://nexla-271516.uc.r.appspot.com/"
     private const val BASE_URL = "http://192.168.56.1:80/"
 
     fun getInstance(): Retrofit{
+        val client = OkHttpClient.Builder()
+            .connectTimeout(60, TimeUnit.SECONDS)
+            .writeTimeout(60, TimeUnit.SECONDS)
+            .readTimeout(60, TimeUnit.SECONDS)
+            .build()
+
         return Retrofit.Builder()
             .baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
+            .client(client)
             .build()
     }
 }
